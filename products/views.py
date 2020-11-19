fca4f45ecd54bf2de16ff82b538dc2b1e36d89de
PeterKellett <peterwkellett@gmail.com>
2020-11-19 19:08:34 +0000

    Added side bar populated with categories

--- products/views.py
+++ products/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Category
 
 
 # Create your views here.
 def all_products(request):
     """A view to show all products including sorting and search queries"""
     products = Product.objects.all()
+    categories = Category.objects.all()
     context = {
         'products': products,
+        'categories': categories
     }
     return render(request, 'products/products.html', context)
