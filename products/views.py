48954e8269d9b988083962eeae44e487a0ce2d68
PeterKellett <peterwkellett@gmail.com>
2020-12-09 16:55:38 +0000

    Attempt at filtering using session variables.

--- products/views.py
+++ products/views.py
@@ -1,66 +1,72 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.contrib import sessions
 from django.db.models import Q
 from .models import Product, Category
 
 
 # Create your views here.
 def all_products(request):
+    request.session['color'] = 'blue'
+    sess = request.session
+    print(sess)
+    print("color = ")
+    print(request.session['color'])
     """A view to show all products including sorting and search queries"""
     products = Product.objects.all()
     categories = Category.objects.all()
 
-    query = None
-    current_category = None
-
-    small = None
     medium = None
     large = None
     xlarge = None
+    query = None
+    current_category = None
 
     if request.GET:
-        small = request.GET['small']
-        if small == 'False':
-            small = False
-            small_queryset = products.filter(size_s=None)
-            print("exclude small_queryset")
-            print(small_queryset)
-        else:
-            small_queryset = products.filter(size_s=True)
-            print("include small_queryset")
-            print(small_queryset)
-        medium = request.GET['medium']
-        if medium == 'False':
-            medium = False
-            medium_queryset = products.filter(size_m=None)
-            print("exclude medium_queryset")
-            print(medium_queryset)
-        else:
-            medium_queryset = products.filter(size_m=True)
-            print("include medium_queryset")
-            print(medium_queryset)
+        if 'medium' in request.GET:
+            small = request.session['small']
+            print(small)
+            if small == 'False':
+                request.session['small'] = False
+                small_queryset = products.filter(size_s=None)
+                print("exclude small_queryset")
+                print(small_queryset)
+            else:
+                small_queryset = products.filter(size_s=True)
+                print("include small_queryset")
+                print(small_queryset)
+            medium = request.GET['medium']
+            if medium == 'False':
+                medium = False
+                medium_queryset = products.filter(size_m=None)
+                print("exclude medium_queryset")
+                print(medium_queryset)
+            else:
+                medium_queryset = products.filter(size_m=True)
+                print("include medium_queryset")
+                print(medium_queryset)
 
-        large = request.GET['large']
-        if large == 'False':
-            large = False
-            large_queryset = products.filter(size_lg=None)
-            print("exclude large_queryset")
-            print(large_queryset)
-        else:
-            large_queryset = products.filter(size_lg=True)
-            print("include large_queryset")
-            print(large_queryset)
-        xlarge = request.GET['xlarge']
-        if xlarge == 'False':
-            xlarge = False
-            xlarge_queryset = products.filter(size_xl=None)
-            print("exclude xlarge_queryset")
-            print(xlarge_queryset)
-        else:
-            xlarge_queryset = products.filter(size_xl=True)
-            print("include xlarge_queryset")
-            print(xlarge_queryset)
-        products = small_queryset | medium_queryset | large_queryset | xlarge_queryset
+            large = request.GET['large']
+            if large == 'False':
+                large = False
+                large_queryset = products.filter(size_lg=None)
+                print("exclude large_queryset")
+                print(large_queryset)
+            else:
+                large_queryset = products.filter(size_lg=True)
+                print("include large_queryset")
+                print(large_queryset)
+            xlarge = request.GET['xlarge']
+            if xlarge == 'False':
+                xlarge = False
+                xlarge_queryset = products.filter(size_xl=None)
+                print("exclude xlarge_queryset")
+                print(xlarge_queryset)
+            else:
+                xlarge_queryset = products.filter(size_xl=True)
+                print("include xlarge_queryset")
+                print(xlarge_queryset)
+            products = small_queryset | medium_queryset | large_queryset | xlarge_queryset
         print("products result")
         print(products)
 
@@ -79,13 +85,16 @@ def all_products(request):
             current_category = request.GET['category'].split(',')
             products = products.filter(category__name__in=current_category)
             current_category = request.GET['category']
-
+    else:
+        request.session['small'] = None
+        medium = None
+        large = None
+        xlarge = None
     context = {
         'products': products,
         'search_term': query,
         'categories': categories,
         'current_category': current_category,
-        'small': small,
         'medium': medium,
         'large': large,
         'xlarge': xlarge,
