72eed994c1095032653307bc642bb0f84da902af
PeterKellett <peterwkellett@gmail.com>
2021-01-26 10:52:59 +0000

    Home images context_processor fix.

--- project_4/settings.py
+++ project_4/settings.py
@@ -82,6 +82,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 # Shopping bag context processor
                 'shopping_bag.contexts.shopping_bag_contents',
             ],
