ea674673d9e52d94e9d36201e71889ddbfaab214
PeterKellett <peterwkellett@gmail.com>
2021-01-31 01:14:19 +0000

    Set up basic views and urls.

--- site_management/urls.py
+++ site_management/urls.py
@@ -3,7 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.site_management, name='site_management'),
-    path('site_management/products',
+    path('products/',
          views.site_management_products,
          name='site_management_products'),
+    path('orders/',
+         views.site_management_orders,
+         name='site_management_orders'),
+    path('reviews/',
+         views.site_management_reviews,
+         name='site_management_reviews'),
 ]
--- site_management/views.py
+++ site_management/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from products.models import Product, Category
 from products.forms import ProductForm
+from checkout.models import Order, OrderLineItem
+from profiles.models import Reviews
 
 
 # Create your views here.
@@ -12,8 +14,24 @@ def site_management(request):
 
 
 def site_management_products(request):
-    products = Product.objects.all().order_by('name')
+    products = Product.objects.all()
     context = {
         'products': products,
     }
     return render(request, 'site_management/products.html', context)
+
+
+def site_management_orders(request):
+    orders = Order.objects.all()
+    context = {
+        'orders': orders,
+    }
+    return render(request, 'site_management/orders.html', context)
+
+
+def site_management_reviews(request):
+    reviews = Reviews.objects.all()
+    context = {
+        'reviews': reviews,
+    }
+    return render(request, 'site_management/reviews.html', context)
